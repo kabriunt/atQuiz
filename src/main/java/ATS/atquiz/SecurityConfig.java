77e47ff80e81cc84a899c5b88686291a6685a97f
dcabrera <dcabrera@AT-GF1V5M2.atsistemas.lan>
2018-02-07 14:16:40 +0100

    Capa controlador y SecurityConfig

---
+++ src/main/java/ATS/atquiz/SecurityConfig.java
@@ -0,0 +1,37 @@
+package ATS.atquiz;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableAutoConfiguration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	DataSource dataSource;
+
+	@Autowired
+	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery("")
+				.authoritiesByUsernameQuery("");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/", "/home").permitAll()
+		.antMatchers("/admin").hasRole("ADMIN")
+		.antMatchers("/candidate").hasRole("CANDIDATE")
+				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
+				.permitAll();
+		http.exceptionHandling().accessDeniedPage("/403");
+	}
+
+}
---
+++ src/main/java/ATS/atquiz/controller/UserController.java
@@ -0,0 +1,5 @@
+package ATS.atquiz.controller;
+
+public class UserController {
+
+}
