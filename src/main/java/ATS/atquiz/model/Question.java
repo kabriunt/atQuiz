8ee918163dac87e52f13eb5dede38b0b25554568
ftorrejon <ftorrejon@AT-8J1V5M2.atsistemas.lan>
2018-02-07 13:37:55 +0100

    Model Question

--- src/main/java/ATS/atquiz/model/Question.java
+++ src/main/java/ATS/atquiz/model/Question.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 
