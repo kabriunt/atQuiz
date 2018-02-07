1625eb5162680762c8091cfc7fd96181298098aa
ajsanchez.roldan <ajsanchez.roldan@AT-18WFLH2.atsistemas.lan>
2018-02-07 15:23:39 +0100

    UserController con funcionalidad

--- src/main/java/ATS/atquiz/controller/UserController.java
+++ src/main/java/ATS/atquiz/controller/UserController.java
@@ -1,5 +1,48 @@
 package ATS.atquiz.controller;
 
-public class UserController {
+import java.util.List;
 
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import ATS.atquiz.dto.UserDto;
+
+import ATS.atquiz.service.User.UserService;
+
+@RestController
+@RequestMapping(value="/api/user")
+public class UserController {
+	
+	@Autowired
+	UserService userService;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<UserDto> getAll(){
+		return userService.findAll();
+	}
+	
+	@RequestMapping(value = "/{oId}", method = RequestMethod.GET)
+	public UserDto getById(@PathVariable(value = "oId") ObjectId oId) {
+		return userService.findById(oId);
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public UserDto create(@RequestBody UserDto userDto) {
+		return userService.create(userDto);
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.PUT)
+	public void update(@RequestBody UserDto userDto) {
+		userService.update(userDto);
+	}
+	
+	@RequestMapping(value = "/{oId}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable(value = "oId") ObjectId oId) {
+		userService.delte(oId);
+	}
 }
