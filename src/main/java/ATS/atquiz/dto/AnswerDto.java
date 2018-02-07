dfcc12bad757fc30c04e445025c18b114a48dfcd
dcabrera <dcabrera@AT-GF1V5M2.atsistemas.lan>
2018-02-07 16:54:29 +0100

    Answer Dto

--- src/main/java/ATS/atquiz/dto/AnswerDto.java
+++ src/main/java/ATS/atquiz/dto/AnswerDto.java
@@ -1,5 +1,23 @@
 package ATS.atquiz.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AnswerDto {
 
+	private String id;
+	
+	private String answer;
+	
+	private boolean ok;
+
+	public AnswerDto() {}
+	
+	public AnswerDto(String id, String answer, boolean ok) {
+		this.id = id;
+		this.answer = answer;
+		this.ok = ok;
+	}
 }
