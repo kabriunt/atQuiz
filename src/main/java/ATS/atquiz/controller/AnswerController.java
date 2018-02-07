c26c23a2ee9166eb29176930aa7840d4ac5ee62b
dcabrera <dcabrera@AT-GF1V5M2.atsistemas.lan>
2018-02-07 15:25:00 +0100

    Answer Controller

--- src/main/java/ATS/atquiz/controller/AnswerController.java
+++ src/main/java/ATS/atquiz/controller/AnswerController.java
@@ -1,5 +1,50 @@
 package ATS.atquiz.controller;
 
+import java.util.List;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import ATS.atquiz.dto.AnswerDto;
+import ATS.atquiz.service.Answer.AnswerService;
+
+@RestController
+@RequestMapping(value="/api/answer")
 public class AnswerController {
 
+	@Autowired
+	private AnswerService answerService;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public List<AnswerDto> findAll(@RequestParam(required=false, defaultValue="0") Integer page, @RequestParam(required=false, defaultValue="5") Integer size){
+		return answerService.findAll(page, size);
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/{idAnswer}")
+	public AnswerDto findById(@PathVariable ObjectId idAnswer) throws Exception {
+		return answerService.findById(idAnswer);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public AnswerDto create(@RequestBody AnswerDto a) {
+		return answerService.create(a);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT, value = "/{idAnswer}")
+	public void update(@PathVariable ObjectId idAnswer, @RequestBody AnswerDto a) {
+		answerService.update(a);
+	}
+	
+	@RequestMapping(method = RequestMethod.DELETE, value = "/{idAnswer}")
+	public void delete(@PathVariable ObjectId idAnswer, @RequestBody AnswerDto a) {
+		answerService.delete(idAnswer);
+	}
+	
+	
 }
