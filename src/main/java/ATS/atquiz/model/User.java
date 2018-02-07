10de27eb26a2a97fe1f37524dd8e62b05b07f6a3
ajsanchez.roldan <ajsanchez.roldan@AT-18WFLH2.atsistemas.lan>
2018-02-07 13:34:19 +0100

    Model User

--- src/main/java/ATS/atquiz/model/User.java
+++ src/main/java/ATS/atquiz/model/User.java
@@ -1,5 +1,27 @@
 package ATS.atquiz.model;
 
-public class User {
+import java.io.Serializable;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Document
+public class User implements Serializable{
+
+	private static final long serialVersionUID = 1827185422867016710L;
+
+	@Id
+	private ObjectId id;
+	
+	@Indexed(unique = true)
+	private String username;
+	
+	private String password;
 }
