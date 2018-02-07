a4ea43e2160b24bce80908805d9369ae5b165537
Daniel Cabrera López <dcabrera@atsistemas.com>
2018-02-07 12:57:43 +0100

    Comienzo de proyecto

---
+++ src/main/java/ATS/atquiz/AtquizApplication.java
@@ -0,0 +1,12 @@
+package ATS.atquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtquizApplication.class, args);
+	}
+}
