71f8445f635e9e022b092aa4f913b71b63414f6c
dcabrera <dcabrera@AT-GF1V5M2.atsistemas.lan>
2018-02-07 13:34:15 +0100

    Answer model

--- src/main/java/ATS/atquiz/model/Answer.java
+++ src/main/java/ATS/atquiz/model/Answer.java
@@ -1,5 +1,26 @@
 package ATS.atquiz.model;
 
-public class Answer {
+import java.io.Serializable;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Document
+public class Answer implements Serializable{
+	
+	private static final long serialVersionUID = -7799201071771170916L;
+
+	@Id
+	private ObjectId id;
+	
+	private String texto;
+	
+	private boolean ok;
+	
 }
