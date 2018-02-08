31e40d3b8c46326171e30995d3eb50851051d0a7
ftorrejon <ftorrejon@AT-8J1V5M2.atsistemas.lan>
2018-02-08 17:17:53 +0100

    Controller

--- src/main/java/ATS/atquiz/controller/QuestionController.java
+++ src/main/java/ATS/atquiz/controller/QuestionController.java
@@ -2,7 +2,6 @@ package ATS.atquiz.controller;
 
 import java.util.List;
 
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
