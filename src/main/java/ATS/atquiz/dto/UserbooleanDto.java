79664c003548bcf2ab3599e363844e35b242f85a
ftorrejon <ftorrejon@AT-8J1V5M2.atsistemas.lan>
2018-02-09 10:33:20 +0100

    Creación UserBooleanDto

---
+++ src/main/java/ATS/atquiz/dto/UserbooleanDto.java
@@ -0,0 +1,42 @@
+package ATS.atquiz.dto;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserbooleanDto implements Serializable{
+	
+	
+	private static final long serialVersionUID = 6387997671995409651L;
+
+	private String id;
+	
+	private String username;
+	
+	private String password;
+	
+	private boolean nonExpiredAccount;
+	
+	private boolean nonLocked;
+	
+	private boolean nonExpiredCredentials;
+	
+	private boolean enabled;
+	
+	public UserbooleanDto(){}
+	
+	public UserbooleanDto(String id, String username, String password,
+			boolean nonExpiredAccount, boolean nonLocked, boolean nonExpiredCredentials, boolean enabled) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.nonExpiredAccount = nonExpiredAccount;
+		this.nonLocked = nonLocked;
+		this.nonExpiredCredentials = nonExpiredCredentials;
+		this.enabled = enabled;
+	}
+	
+}
