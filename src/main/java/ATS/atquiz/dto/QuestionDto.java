668f3a4c8acf69b1c9783e9e5f776e42de50621c
ftorrejon <ftorrejon@AT-8J1V5M2.atsistemas.lan>
2018-02-09 11:43:02 +0100

    Question Dto

--- src/main/java/ATS/atquiz/dto/QuestionDto.java
+++ src/main/java/ATS/atquiz/dto/QuestionDto.java
@@ -1,5 +1,28 @@
 package ATS.atquiz.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class QuestionDto {
+	
+	private String id;
+	
+	private String question;
+	
+	private String tag;
+	
+	private String level;
+	
+	public QuestionDto() {}
+	
+	public QuestionDto(String id, String question, String tag, String level) {
+		this.id = id;
+		this.question = question;
+		this.tag = tag;
+		this.level = level;
+	}
+	
 
 }
