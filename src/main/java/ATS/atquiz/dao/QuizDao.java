06653c8245aafe5a879367d7e5e9b69bc3a46f45
dcabrera <dcabrera@AT-GF1V5M2.atsistemas.lan>
2018-02-09 11:38:59 +0100

    Quiz Controller, Dao y Service

---
+++ src/main/java/ATS/atquiz/dao/QuizDao.java
@@ -0,0 +1,16 @@
+package ATS.atquiz.dao;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import ATS.atquiz.model.Quiz;
+
+@Repository
+public interface QuizDao extends MongoRepository<Quiz, String>{
+
+	@Query("{ 'username' : ?0 }")
+	public List<Quiz> findByUsername(String username);
+}
