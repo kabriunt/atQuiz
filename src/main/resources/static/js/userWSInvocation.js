ed5512fa1dfe14e21bfd2dafd7ee3141ac9f2191
ajsanchez.roldan <ajsanchez.roldan@AT-18WFLH2.atsistemas.lan>
2018-02-19 13:52:45 +0100

    mejora de código

--- src/main/resources/static/js/userWSInvocation.js
+++ src/main/resources/static/js/userWSInvocation.js
@@ -22,7 +22,7 @@ function getAll(){
 			});
 			html+='</tbody></table>';
 			//se pasa el contenido a la vista
-			$("#content").html(html);
+			$("#users").html(html);
 		},		
 		//esto se hará en caso de que el servicio web falle
 		error:function(res){
@@ -50,6 +50,55 @@ function getUser(){
 		});
 	}
 }
+
+function newUser(){
+	//Nos traemos los datos del formulario
+	var username = $("#username").val();
+	var password = $("#password").val();
+	var role = $("#role").val();	
+	var name = $("#name").val();
+	var surname = $("#surname").val();
+    var email = $("#email").val();	
+	var dni = $("#dni").val();
+	var nonExpiredAccount = $("#nonExpiredAccount").is(":checked");
+	var nonLocked = $("#nonLocked").is(":checked");
+	var nonExpiredCredentials = $("#nonExpiredCredentials").is(":checked");
+	var enabled = $("#enabled").is(":checked");
+	$.ajax( {
+		type:"POST",
+		url:"http://localhost:8081/api/user/admin",
+		contentType:"application/json",
+		dataType:"text",
+		//creamos el json que recibe el servicio web
+		data:JSON.stringify( {"username": username,"password": password,
+			"role": role,"name":name, "surname": surname,
+			"email": email,"dni": dni, "nonExpiredAccount":nonExpiredAccount,
+	      "nonLocked": nonLocked, "nonExpiredCredentials": nonExpiredCredentials, "enabled": enabled}),
+	    success:function(data){
+	    	var html="Usuario registrado con éxito";
+	    	$('#responseForCreation').html(html);
+	    },
+	    error:function(res){
+	    	alert("ERROR "+ res.statusText); }
+	});
+}
+
+function deleteUser(){
+	var id = $('#idDelete').val();
+	id = id.replace(/\s+/g, '');
+	$.ajax({
+		type:"DELETE",
+		url:"http://localhost:8081/api/user/admin/"+id,
+		contentType:"text",
+		success:function(data){
+			var html = "Se ha borrado el usuario";
+			$('#responseForDelete').html(html);
+		},
+		error:function(res){
+		alert("Error "+res.statusText);}
+	});
+}
+
 
 function hideUsers() {
     var x = document.getElementById("userTable");
@@ -98,52 +147,3 @@ function hideDelete() {
 		y.firstChild.data="+";
     }
 }
-
-function newUser(){
-	//Nos traemos los datos del formulario
-	var username = $("#username").val();
-	var password = $("#password").val();
-	var role = $("#role").val();	
-	var name = $("#name").val();
-	var surname = $("#surname").val();
-    var email = $("#email").val();	
-	var dni = $("#dni").val();
-	var nonExpiredAccount = $("#nonExpiredAccount").is(":checked");
-	var nonLocked = $("#nonLocked").is(":checked");
-	var nonExpiredCredentials = $("#nonExpiredCredentials").is(":checked");
-	var enabled = $("#enabled").is(":checked");
-	$.ajax( {
-		type:"POST",
-		url:"http://localhost:8081/api/user/admin",
-		contentType:"application/json",
-		dataType:"text",
-		//creamos el json que recibe el servicio web
-		data:JSON.stringify( {"username": username,"password": password,
-			"role": role,"name":name, "surname": surname,
-			"email": email,"dni": dni, "nonExpiredAccount":nonExpiredAccount,
-	      "nonLocked": nonLocked, "nonExpiredCredentials": nonExpiredCredentials, "enabled": enabled}),
-	    success:function(data){
-	    	var html="Usuario registrado con éxito";
-	    	$('#content2').html(html);
-	    },
-	    error:function(res){
-	    	alert("ERROR "+ res.statusText); }
-	});
-}
-
-function deleteUser(){
-	var id = $('#idDelete').val();
-	id = id.replace(/\s+/g, '');
-	$.ajax({
-		type:"DELETE",
-		url:"http://localhost:8081/api/user/admin/"+id,
-		contentType:"text",
-		success:function(data){
-			var html = "Se ha borrado el usuario";
-			$('#content3').html(html);
-		},
-		error:function(res){
-		alert("Error "+res.statusText);}
-	});
-}
-
