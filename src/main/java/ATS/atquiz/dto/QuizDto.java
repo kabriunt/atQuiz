0ef569fb93ff5caaf6634359fe1f91e1b73f2268
ajsanchez.roldan <ajsanchez.roldan@AT-18WFLH2.atsistemas.lan>
2018-02-20 13:01:48 +0100

    refactoring

--- src/main/java/ATS/atquiz/dto/QuizDto.java
+++ src/main/java/ATS/atquiz/dto/QuizDto.java
@@ -17,7 +17,7 @@ public class QuizDto implements Serializable{
 	
 	private static final long serialVersionUID = -2497382476162599801L;
 
-	public String Id;
+	public String id;
 	
 	@DateTimeFormat(pattern = "yyyy/MM/dd hh:mm:ss")
 	public Date dateIni;
@@ -25,7 +25,7 @@ public class QuizDto implements Serializable{
 	@DateTimeFormat(pattern = "yyyy/MM/dd hh:mm:ss")
 	public Date dateEnd;
 	
-	Double result;
+	Double mark;
 	
 	private User user;
 	
@@ -33,11 +33,11 @@ public class QuizDto implements Serializable{
 	
 	public QuizDto() {}
 	
-	public QuizDto(String Id, Date dateIni, Date dateEnd, Double result, User user, List<QuestionDto> questions) {
-		this.Id = Id;
+	public QuizDto(String id, Date dateIni, Date dateEnd, Double mark, User user, List<QuestionDto> questions) {
+		this.id = id;
 		this.dateIni = dateIni;
 		this.dateEnd = dateEnd;
-		this.result = result;
+		this.mark = mark;
 		this.user = user;
 		this.questions = questions;
 	}
