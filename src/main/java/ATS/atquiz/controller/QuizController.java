e3a8813fbd88018b58feb04ce8a3a21b5cc8f5ae
ajsanchez.roldan <ajsanchez.roldan@AT-18WFLH2.atsistemas.lan>
2018-02-20 14:11:22 +0100

    correción quiz controller, función findQuizsByUsername parametro de filtro como requestParam (antes estaba como pathVariable)

--- src/main/java/ATS/atquiz/controller/QuizController.java
+++ src/main/java/ATS/atquiz/controller/QuizController.java
@@ -3,7 +3,6 @@ package ATS.atquiz.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,8 +26,8 @@ public class QuizController {
 		return quizService.findAll();
 	}
 	
-	@RequestMapping(value = "/admin/{username}", method = RequestMethod.GET)
-	public List<QuizDto> findByUsername(@PathVariable String username) throws NotFoundException{
+	@RequestMapping(value = "/admin/quizs", method = RequestMethod.GET)
+	public List<QuizDto> findQuizsByUsername(@RequestParam String username) throws NotFoundException{
 		return quizService.findByUsername(username);
 	}
 	
